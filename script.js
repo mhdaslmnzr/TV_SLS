1c18930d66854ce28f25ad051a13b3c6e6b2b795
mhdaslmnzr <aslam99955@gmail.com>
2022-04-28 13:32:36 +0530

    two

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
-var subheadings = [
-  "• US Politics",
-  "• Impeachment",
-  "• Global News",
-  "• Global Markets & Economy"
-];
-var news = [
-  ["• Biden kills 5000 jobs by canceling the border wall construction",
-  "• US pauses arms sales",
-  "• Wyoming sues Biden Govt",
-  "• McCabe Aware of Exculpatory Evidence",
-  "• End of US Politics"],
-  ["• DHS sees a heightened threat",
-  "• US pauses arms sales",
-  "• Wyoming sues Biden Govt.",
-  "• McCabe Aware of Exculpatory Evidence",
-  "• End of Impeachment"],
-  ["• India to send soldiers to Russia for S-400 training",
-  "• US pauses arms sales",
-  "• Wyoming sues Biden Govt.",
-  "• McCabe Aware of Exculpatory Evidence",
-  "• End of Global News"],
-  ["• All indices down by at least 2.6%",
-  "• US pauses arms sales",
-  "• Wyoming sues Biden Govt.",
-  "• McCabe Aware of Exculpatory Evidence",
-  "• End of Global Markets"]
-];
+// var subheadings = [
+//   "• US Politics",
+//   "• Impeachment",
+//   "• Global News",
+//   "• Global Markets & Economy"
+// ];
+// var news = [
+//   ["• Biden kills 5000 jobs by canceling the border wall construction",
+//   "• US pauses arms sales",
+//   "• Wyoming sues Biden Govt",
+//   "• McCabe Aware of Exculpatory Evidence",
+//   "• End of US Politics"],
+//   ["• DHS sees a heightened threat",
+//   "• US pauses arms sales",
+//   "• Wyoming sues Biden Govt.",
+//   "• McCabe Aware of Exculpatory Evidence",
+//   "• End of Impeachment"],
+//   ["• India to send soldiers to Russia for S-400 training",
+//   "• US pauses arms sales",
+//   "• Wyoming sues Biden Govt.",
+//   "• McCabe Aware of Exculpatory Evidence",
+//   "• End of Global News"],
+//   ["• All indices down by at least 2.6%",
+//   "• US pauses arms sales",
+//   "• Wyoming sues Biden Govt.",
+//   "• McCabe Aware of Exculpatory Evidence",
+//   "• End of Global Markets"]
+// ];
 
-for (i = 0; i < subheadings.length; i++) {
-  $("#newsTicker p").append(
-    "<span class='section'; style='color:#E8F9C5'>" + subheadings[i] + "</span>"
-  );
+// for (i = 0; i < subheadings.length; i++) {
+//   $("#newsTicker p").append(
+//     "<span class='section'; style='color:#E8F9C5'>" + subheadings[i] + "</span>"
+//   );
 
-for (j = 0; j < news[i].length; j++) {
-  $("#newsTicker p").append(
+// for (j = 0; j < news[i].length; j++) {
+//   $("#newsTicker p").append(
     
-    "<span class='story'>"  + news[i][j] + "</span>"
-  );
-}
-}
-
-
-// var dt = new Date();
-// document.getElementById('date-time').innerHTML=dt;
+//     "<span class='story'>"  + news[i][j] + "</span>"
+//   );
+// }
+// }
